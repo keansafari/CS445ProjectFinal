a121a8457d7a929163ff8814e1db1235b2b7a57e
Lenny Yang <lennyiswise@gmail.com>
2017-11-07 19:53:02 -0800

    first

---
+++ src/cs445programfinal/Vector3Float.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs445programfinal;
+
+/**
+ *
+ * @author lennyyang
+ */
+
+public class Vector3Float {
+    public float x, y, z;
+    public Vector3Float(int x, int y, int z){
+    this.x=x;
+    this.y=y;
+    this.z=z;
+    }
+}
